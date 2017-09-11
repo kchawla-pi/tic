a3135835063513dc6ee252eee3497d8130c0b9a0
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2017-09-11 16:26:01 -0400

    Changed: local_path_root is now system independent

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import git
 import os
 
 
-local_path_root = 'C:\\Users\\kshit\\Dropbox\\workspace\\tic\\tic\\'
+local_path_root = os.path.split(__file__)[0] #'C:\\Users\\kshit\\Dropbox\\workspace\\tic\\tic\\'
 repo_url_root = 'https://github.com/theimagingcollective'
 repos = ['read_dicom', 'slice_timing']
 extension = '.git'
 
 for repo_ in repos:
-	target_dir = os.path.join(local_path_root, repo_)
+	target_dir = os.path.realpath(os.path.join(local_path_root, repo_))
 	repo_url = ''.join([repo_url_root, '/', repo_, extension])
 	try:
 		git.Repo.clone_from(repo_url, target_dir)
